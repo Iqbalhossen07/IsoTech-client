bdbb4522aaa41dc9737493bdb983425df653b364
mdiqbalkhan <iqbalhossen0711@gmail.com>
2023-10-20 16:34:48 +0600

    6 different brand type card added

--- src/componets/Banner/Banner.jsx
+++ src/componets/Banner/Banner.jsx
@@ -9,8 +9,8 @@ const Banner = () => {
             <div>
                  <div className='flex items-center gap-2'>
                     <img className='w-12 ' data-aos="fade-up-right" alt="" />
-                    <p className='italic mb-5 text-pink-600' data-aos="fade-right">Ekahne kichu likhet hobe</p></div>
-                <h1 className="mb-5 text-2xl md:text-3xl lg:text-6xl font-medium" data-aos="fade-left">Sale 20% Off
+                    <p className='italic mb-5 text-2xl text-[#54e2d2]' data-aos="fade-right">Offer! Offer!</p></div>
+                <h1 className="mb-5 text-2xl md:text-3xl lg:text-6xl font-medium" data-aos="fade-left">Sale <span className='text-[#54e2d2]'>20%</span> Off
 On Everything</h1>
                 <p className='italic mb-5' data-aos="flip-left"></p>
                 
